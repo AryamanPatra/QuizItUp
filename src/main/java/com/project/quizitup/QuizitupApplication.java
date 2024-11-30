674b26fb3f5c0ccce6c15b591fc29a5b24930fce
Aryaman Patra <aryamanprivate26@gmail.com>
2024-12-01 01:36:06 +0530

    First commit. Project created

---
+++ src/main/java/com/project/quizitup/QuizitupApplication.java
@@ -0,0 +1,13 @@
+package com.project.quizitup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizitupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizitupApplication.class, args);
+	}
+
+}
